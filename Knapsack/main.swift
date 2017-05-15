d02d6ac7cd006beb0e0ea5c6e6114ae6cec7df01
Jan Posz <janek@vorm.io>
2017-05-15 10:30:00 +0200

    Feature: - knapsack item provider

--- Knapsack/main.swift
+++ Knapsack/main.swift
@@ -8,48 +8,12 @@
 
 import Foundation
 
-
-func items() -> [KnapsackItem] {
-    return [KnapsackItem(cost: 181, weight: 11121),
-            KnapsackItem(cost: 212, weight: 121231),
-            KnapsackItem(cost: 33, weight: 131123),
-            KnapsackItem(cost: 424, weight: 141231),
-            KnapsackItem(cost: 535, weight: 1521),
-            KnapsackItem(cost: 161, weight: 11211),
-            KnapsackItem(cost: 242, weight: 121),
-            KnapsackItem(cost: 33, weight: 1321),
-            KnapsackItem(cost: 454, weight: 3141),
-            KnapsackItem(cost: 55, weight: 1451),
-            KnapsackItem(cost: 151, weight: 1151),
-            KnapsackItem(cost: 232, weight: 121),
-            KnapsackItem(cost: 3883, weight: 1631),
-            KnapsackItem(cost: 434, weight: 14871),
-            KnapsackItem(cost: 55, weight: 151),
-            KnapsackItem(cost: 151, weight: 1151),
-            KnapsackItem(cost: 232, weight: 1321),
-            KnapsackItem(cost: 33, weight: 1351),
-            KnapsackItem(cost: 44, weight: 1431),
-            KnapsackItem(cost: 545, weight: 1651),
-            KnapsackItem(cost: 13451, weight: 1161),
-            KnapsackItem(cost: 22, weight: 1231),
-            KnapsackItem(cost: 3553, weight: 4131),
-            KnapsackItem(cost: 44, weight: 1461),
-            KnapsackItem(cost: 525, weight: 1751),
-            KnapsackItem(cost: 121, weight: 1131),
-            KnapsackItem(cost: 2352, weight: 1261),
-            KnapsackItem(cost: 3223, weight: 1351),
-            KnapsackItem(cost: 444, weight: 1461),
-            KnapsackItem(cost: 5335, weight: 1531),
-            KnapsackItem(cost: 1641, weight: 1113),
-            KnapsackItem(cost: 2552, weight: 1271),
-            KnapsackItem(cost: 3463, weight: 1531),
-            KnapsackItem(cost: 4754, weight: 7141),
-            KnapsackItem(cost: 555, weight: 1512)]
-}
+let weight = 5000
+let items = KnapsackItemProvider.itemsWith(count: 99)
 
 // Dynamic programming solution
 
-let dynamicSolver = KnapsackDynamicSolver(maximumWeight: 8000, availableItems: items())
+let dynamicSolver = KnapsackDynamicSolver(maximumWeight: weight, availableItems: items)
 let dynamicResult = dynamicSolver.solve()
 print(dynamicResult)
 
@@ -59,7 +23,7 @@ if let solveTime = dynamicSolver.solveTime {
 
 // Naive solution
 
-let naiveSolver = KnapsackNaiveSolver(maximumWeight: 8000, availableItems: items())
+let naiveSolver = KnapsackNaiveSolver(maximumWeight: weight, availableItems: items)
 let naiveResult = naiveSolver.solve()
 print(naiveResult)
 
